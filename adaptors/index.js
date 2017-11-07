75ac6e87e76a97a3a2120126b3a30b21ff3c39f3
kylewstewart <kyle.w.stewart@mac.com>
2017-11-07 07:00:49 -0500

    Index.js rollback reformating

--- adaptors/index.js
+++ adaptors/index.js
@@ -2,6 +2,27 @@
 const baseURL = 'http://localhost:3000/api/v1';
 // const baseURL = 'https://dgmvlktrln.localtunnel.me/api/v1';
 
+export const getCode = mobile => (
+  fetch(`${baseURL}/users`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ mobile }),
+  }).then(response => response.json())
+);
+
+export const auth = (userId, code) => (
+  fetch(`${baseURL}/auth`, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ userId, code }),
+  }).then(response => response.json())
+);
 
 export const currentUser = token => (
   fetch(`${baseURL}/current_user`, {
